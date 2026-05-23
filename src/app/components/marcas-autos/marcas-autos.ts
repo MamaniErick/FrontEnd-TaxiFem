7158d2704e676de27dfd6da2537b70d14af81c05
MamaniErick <mamanierick64@gmail.com>
2026-05-23 19:19:16 -0300

    Codigo para Mostrar Modelos Según Marca

--- src/app/components/marcas-autos/marcas-autos.ts
+++ src/app/components/marcas-autos/marcas-autos.ts
@@ -13,6 +13,10 @@ import { CommonModule } from '@angular/common';
 export class MarcasAutos {
 
   marcasAuto: Array<any> = [];
+  modelos: Array<any> = [];
+
+  marcaSeleccionadaName: string = '';
+  cacheModelos: { [key: number]: Array<any> } = {};
 
   constructor(private marcasAutoService: MarcasAutoService,
     private cdr: ChangeDetectorRef) { }
@@ -32,6 +36,32 @@ export class MarcasAutos {
         console.log(error)
       }
     )
+  }
 
+  obtenerModelos(id: number, name: string) {
+    this.marcaSeleccionadaName = name;
+    if (this.cacheModelos[id]) {
+      console.log(`Cargando modelos de ${name} desde la memoria local (0 transacciones consumidas)`);
+      this.modelos = this.cacheModelos[id];
+      this.cdr.detectChanges();
+    } else {
+        console.log(`Consultando a internet los modelos de ${name}...`);
+        this.marcasAutoService.getModelosId(id).subscribe(
+        (result)=>{
+          this.modelos = result;
+          this.cacheModelos[id] = result; // 💾 Los guardamos en la memoria para la próxima
+          console.log(`Modelos guardados en caché para ${name}:`, result);
+          this.cdr.detectChanges();},
+        (error)=>{
+          console.log(error)
+        }
+      )
+    }
+  }
+
+  cerrarModal() {
+    this.marcaSeleccionadaName = '';
+    this.modelos = []; // Al vaciar el array, el contenido interno desaparece
+    this.cdr.detectChanges();
   }
 }
--- src/app/servicios/marcas-auto.service.ts
+++ src/app/servicios/marcas-auto.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class MarcasAutoService {
 
   constructor(private http: HttpClient) { }
-  
+
   getMarcasAuto(): Observable<any> {
     let httpOptions = {
       headers: new HttpHeaders({
@@ -18,5 +18,16 @@ export class MarcasAutoService {
       })
     }
     return this.http.get('https://car-specs.p.rapidapi.com/v2/cars/makes', httpOptions)
+  }
+
+  getModelosId(id:number):Observable<any> {
+    let httpOptions = {
+      headers: new HttpHeaders({
+        'x-rapidapi-key': 'db241b6b7bmsh019c06a40db46cfp1c0838jsn188e70e69af9',
+        'x-rapidapi-host': 'car-specs.p.rapidapi.com',
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.get('https://car-specs.p.rapidapi.com/v2/cars/makes/'+id+'/models', httpOptions)
   }
 }
