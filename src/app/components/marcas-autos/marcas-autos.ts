939f870cec342236f5e9699bd295e45e895e4988
MamaniErick <mamanierick64@gmail.com>
2026-05-23 18:18:59 -0300

    Codigo para obtener Marcas

--- src/app/components/marcas-autos/marcas-autos.ts
+++ src/app/components/marcas-autos/marcas-autos.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { MarcasAutoService } from '../../servicios/marcas-auto.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-marcas-autos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './marcas-autos.html',
   styleUrl: './marcas-autos.css',
 })
-export class MarcasAutos {}
+export class MarcasAutos {
+
+  marcasAuto: Array<any> = [];
+
+  constructor(private marcasAutoService: MarcasAutoService,
+    private cdr: ChangeDetectorRef) { }
+
+  ngOnInit(): void {
+    this.obtenerMarcasAuto();
+  }
+
+  obtenerMarcasAuto() {
+    this.marcasAutoService.getMarcasAuto().subscribe(
+      (result)=>{
+        this.marcasAuto = result,
+        console.log(result),
+        this.cdr.detectChanges();
+      },
+      (error)=>{
+        console.log(error)
+      }
+    )
+
+  }
+}
