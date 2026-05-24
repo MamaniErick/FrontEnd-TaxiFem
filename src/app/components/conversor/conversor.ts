267f15857f728678d41c3af4640e9bd4e67f7f2e
MamaniErick <mamanierick64@gmail.com>
2026-05-24 02:23:54 -0300

    Desarrollo de Codigo Conversor

--- src/app/components/conversor/conversor.ts
+++ src/app/components/conversor/conversor.ts
@@ -5,17 +5,12 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-conversor',
-  standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './conversor.html',
   styleUrl: './conversor.css',
 })
 export class Conversor {
-
-  // Acá guardamos el array transformado
   listaMonedas: Array<{ codigo: string, nombre: string }> = [];
-
-  // Variables vinculadas al formulario
   cantidad: number = 1;
   monedaOrigen: string = 'USD';
   monedaDestino: string = 'ARS';
@@ -24,23 +19,18 @@ export class Conversor {
   constructor(
     private conversorService: ConversorService,
     private cdr: ChangeDetectorRef
-  ) { }
-
-  ngOnInit(): void {
+  ) { 
     this.obtenerMondedas();
   }
 
   obtenerMondedas(){
     this.conversorService.getMonedas().subscribe(
       (result)=>{
-        console.log("JSON crudo recibido:", result);
-        
+        console.log(result);        
         if (result && result.currencies) {
-          // Llamamos a nuestro método transformador pasándole el objeto de atributos
-          this.listaMonedas = this.convertirObjetoAArray(result.currencies);
-          
-          console.log("Array de monedas listo para el HTML:", this.listaMonedas);
-          this.cdr.detectChanges(); // Forzamos el renderizado inmediato
+          this.listaMonedas = this.convertirObjetoAArray(result.currencies);          
+          console.log("Lista Monedas:", this.listaMonedas);
+          this.cdr.detectChanges();
         }
       },
       (error)=>{
@@ -49,22 +39,21 @@ export class Conversor {
     )
   }
 
-  // 🧠 2. MÉTODO APARTE: Transforma el objeto de claves/atributos en un Array iterable
   convertirObjetoAArray(currenciesObj: any): Array<{ codigo: string, nombre: string }> {
     return Object.entries(currenciesObj).map(([key, value]) => {
       return {
-        codigo: key,          // Ej: "USD"
-        nombre: String(value)   // Ej: "United States Dollar"
+        codigo: key,          
+        nombre: String(value)
       };
     });
   }
 
-  // 🔄 3. Acción del botón para procesar la conversión matemática mas adelante
   convertirMonto() {
     this.conversorService.getConvertir(this.monedaOrigen, this.monedaDestino, this.cantidad).subscribe(
       (result)=>{
         this.resultado = result.result;
         console.log(result)
+        this.cdr.detectChanges();
       },
       (error)=>{
         console.log(error)
