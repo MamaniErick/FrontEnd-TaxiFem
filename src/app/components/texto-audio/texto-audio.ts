8e533c17861f9c5e6509005de24affbda8d3a2c2
MamaniErick <mamanierick64@gmail.com>
2026-05-24 00:04:15 -0300

    Desarrollo de Codigo

--- src/app/components/texto-audio/texto-audio.ts
+++ src/app/components/texto-audio/texto-audio.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { TextoAudioService } from '../../servicios/texto-audio-service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-texto-audio',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './texto-audio.html',
   styleUrl: './texto-audio.css',
 })
-export class TextoAudio {}
+export class TextoAudio {
+
+  texto: string="";
+  objetUrl: SafeUrl=" ";
+
+  constructor(private textoAudioService: TextoAudioService,
+    private sanitezer: DomSanitizer
+  ){}
+
+  convertirTextoAudio(){
+    this.objetUrl = "";
+   
+    this.textoAudioService.postTextAudio(this.texto).subscribe(
+      data=>{
+        console.log(this.texto)
+        let objectUrl= URL.createObjectURL(data);
+        this.objetUrl= this.sanitezer.bypassSecurityTrustUrl(objectUrl);
+        },
+      error=>{
+        console.log(error)
+      }
+    )
+  }
+}
---
+++ src/app/servicios/texto-audio-service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TextoAudioService {
+
+  constructor(private http: HttpClient) { }
+
+  postTextAudio(text:string): Observable<Blob>{
+    let httpOptions = {
+      headers: new HttpHeaders({
+        'x-rapidapi-key': 'db241b6b7bmsh019c06a40db46cfp1c0838jsn188e70e69af9',
+        'x-rapidapi-host': 'open-ai-text-to-speech1.p.rapidapi.com',
+        'Content-Type': 'application/json' 
+      }),
+      responseType: 'blob' as 'blob',
+      withCredentials: true
+    }
+    let body= {
+      model: 'tts-1',
+	    input: text,
+	    instructions: 'Tono normal',
+	    voice: 'alloy'
+    }
+    return this.http.post('https://open-ai-text-to-speech1.p.rapidapi.com/',body, httpOptions);
+  }
+}
